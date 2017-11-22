f3d65b424c1682f100b2b1c5649efbfdecc9224c
Filip Bounla <oddo4@seznam.cz>
2017-11-22 15:20:22 +0100

    init

---
+++ Xamarin2/Xamarin2/Xamarin2/Contacts.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Contacts : ContentPage
+    {
+        public Contacts()
+        {
+            InitializeComponent();
+        }
+
+        async void Profile_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Profile());
+        }
+    }
+}
